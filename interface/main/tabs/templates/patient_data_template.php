3d035e90100e19add03dd2e9e8c491281b8b8ef7
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2017-05-03 19:24:44 -0400

    Simple proof of concept with static document ID, dynamic patient ID using KO js

--- interface/main/tabs/templates/patient_data_template.php
+++ interface/main/tabs/templates/patient_data_template.php
@@ -30,7 +30,9 @@
                 <strong><i style="margin: 10px 0 0 10px;" class="fa fa-search fa-stack-x" aria-hidden="true"></i></strong>
             </a></span>
             <!-- ko if: patient -->
-                <?php echo "<img src=\"".$GLOBALS['patient_photo_category_name']."\" style='width:30pt;height:30pt;position:absolute;left:50pt;'>"; ?>
+                <?php /*echo "<img src=\"/controller.php?document&retrieve&patient_id=00&document_id=".$GLOBALS['patient_photo_category_name']."&\" style='width:30pt;height:30pt;position:absolute;left:50pt;'>";*/ ?>
+                <?php /*echo "<span data-bind=\"click:refreshPatient,with: patient\"> <img style='width:30pt;height:30pt;position:absolute;left:50pt;' data-bind=\"attr:{src: '/controller.php?document&retrieve&patient_id=' + pubpid() + '&document_id=".$GLOBALS['patient_photo_category_name']."&'}\"></span>";*/ ?>
+                <?php echo "<span data-bind=\"click:refreshPatient,with: patient\"> <img style='width:30pt;height:30pt;position:absolute;left:50pt;' data-bind=\"attr:{src: '/controller.php?document&retrieve&patient_id=' + pubpid() + '&document_id=2&'}\"></span>"; ?>
             <!-- /ko -->
             <div class="patientInfo">
                 <?php echo xlt("Patient"); ?>:
